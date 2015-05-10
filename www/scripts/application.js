17293ce73e327449b05caaff9f7ad969d9ee5061
Caio Andrade <caioertai@gmail.com>
2015-05-10 13:14:31 -0300

    Rounding some edges

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -5,28 +5,8 @@ angular.module('SteroidsApplication', ['supersonic', 'hmTouchEvents'])
   steroids.view.setBackgroundColor("#CCCCCC");
 
   // Table functions
-  $scope.openCombat = function(){
-    $scope.inCombat = true;
-  };
-
-  $scope.openSheet = function(player){
-    $scope.inSheet = true;
-  };
-
-  $scope.closeTable = function(){
-    $scope.inCombat = false;
-    $scope.inSheet = false;
-  };
-
-  // $scope.travel = [0,0,1,0];
-  // $scope.panIntent = function(arrTravel) {
-  //   return arrTravel.indexOf(Math.max.apply(null, arrTravel)); 
-  // };
-
-  // $scope.panEnder = function(player, travel) {
-  //   player.facing[travel.indexOf(Math.max.apply(null, travel))];
-  // };
-  // $scope.panIntent = $scope.travel.indexOf(Math.max.apply(null, $scope.travel)); 
+  $scope.inCombat = false;
+  $scope.inSheet = false;
 
   // Players directions
   var direction = {
@@ -43,18 +23,13 @@ angular.module('SteroidsApplication', ['supersonic', 'hmTouchEvents'])
     west: [direction.right, direction.down, direction.left, direction.up]
   }
 
-  // var north = [direction.up, direction.right, direction.down, direction.left];
-  // var east = [direction.left, direction.up, direction.right, direction.down];
-  // var south = [direction.down, direction.left, direction.up, direction.right];
-  // var west = [direction.right, direction.down, direction.left, direction.up];
-
   $scope.players = [
-    {facing: facing.north, color: '#0087CA', darkColor: '#006ca6', style: 'left: 50%; margin-left: -15vw; bottom: 5%'},
-    {facing: facing.east, color: '#86328C', darkColor: '#642568', style: 'left: 2%; top: 53%; -webkit-transform: rotate(90deg)'},
-    {facing: facing.east, color: '#F15A24', darkColor: '#cc4c1e', style: 'left: 2%; top: 47%; -webkit-transform: rotate(90deg); margin-top: -30vw'},
-    {facing: facing.south, color: '#91be4a', darkColor: '#75993c', style: 'left: 50%; margin-left: -15vw; top: 5%; -webkit-transform: rotate(180deg)'},
-    {facing: facing.west, color: '#c1272d', darkColor: '#991132', style: 'right: 2%; top: 53%; -webkit-transform: rotate(270deg)'},
-    {facing: facing.west, color: '#f7931e', darkColor: '#cf7b19', style: 'right: 2%; top: 47%; -webkit-transform: rotate(270deg); margin-top: -30vw'}
+    {lvl: 1, gear: 0, facing: facing.north, color: '#0087CA', darkColor: '#006ca6', style: 'left: 50%; margin-left: -15vw; bottom: 5%'},
+    {lvl: 1, gear: 0, facing: facing.east, color: '#86328C', darkColor: '#642568', style: 'left: 2%; top: 53%; -webkit-transform: rotate(90deg)'},
+    {lvl: 1, gear: 0, facing: facing.east, color: '#F15A24', darkColor: '#cc4c1e', style: 'left: 2%; top: 47%; -webkit-transform: rotate(90deg); margin-top: -30vw'},
+    {lvl: 1, gear: 0, facing: facing.south, color: '#91be4a', darkColor: '#75993c', style: 'left: 50%; margin-left: -15vw; top: 5%; -webkit-transform: rotate(180deg)'},
+    {lvl: 1, gear: 0, facing: facing.west, color: '#c1272d', darkColor: '#991132', style: 'right: 2%; top: 53%; -webkit-transform: rotate(270deg)'},
+    {lvl: 1, gear: 0, facing: facing.west, color: '#f7931e', darkColor: '#cf7b19', style: 'right: 2%; top: 47%; -webkit-transform: rotate(270deg); margin-top: -30vw'}
   ];
 
 });
