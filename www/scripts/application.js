c3535fa6398b829db6910491114e71d409a7d624
Caio “Ertai” Andrade <caioertai@gmail.com>
2015-09-23 08:27:40 -0300

    New player activating and benching system

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -13,7 +13,7 @@ angular.module('SteroidsApplication', ['supersonic', 'hmTouchEvents'])
   $scope.currentPlayer = 0;
   $scope.playerBonus = 0;
   $scope.monsterPower = 5;
-
+  $scope.menuActive = true;
 
   $scope.monsterTier = function() {
     if ($scope.monsterPower < 8)  {
@@ -53,16 +53,8 @@ angular.module('SteroidsApplication', ['supersonic', 'hmTouchEvents'])
 
   $scope.combatRemove = function(combatant){
     $scope.combatants.splice($scope.combatants.indexOf(combatant), 1);
-  }
 
-  var colors = [
-    ['#0087CA', '#006ca6'],
-    ['#86328C', '#642568'],
-    ['#91be4a', '#75993c'],
-    ['#c1272d', '#991132'],
-    ['#f763a2', '#d9578e'],
-    ['#f7931e', '#cf7b19']
-  ];
+  }
 
   var appWidth      = window.innerWidth;
   var appHeight     = window.innerHeight;
@@ -72,21 +64,40 @@ angular.module('SteroidsApplication', ['supersonic', 'hmTouchEvents'])
   var xMiddle       = appWidth * 0.50 - meepoShift;
   var xRight        = appWidth * 0.85 - meepoShift;
 
-  var yTop          = appHeight * 0.86 - appWidth * 0.135;
-  var yTopMiddle    = appHeight * 0.63 - appWidth * 0.135;
-  var yBottomMiddle = appHeight * 0.37 - appWidth * 0.135;
-  var yBottom       = appHeight * 0.14 - appWidth * 0.135;
+  var yTop          = appHeight * 0.14 - appWidth * 0.135;
+  var yTopMiddle    = appHeight * 0.37 - appWidth * 0.135;
+  var yBottomMiddle = appHeight * 0.63 - appWidth * 0.135;
+  var yBottom       = appHeight * 0.86 - appWidth * 0.135;
+
+
+  $scope.colors = [
+    ['#0087CA', '#006ca6', true],
+    ['#86328C', '#642568', false],
+    ['#91be4a', '#75993c', false],
+    ['#c1272d', '#991132', false],
+    ['#f763a2', '#d9578e', false],
+    ['#f7931e', '#cf7b19', false],
+    ['#999999', '#888888', false],
+  ];
 
   $scope.players = [
-    {lvl: 1, gear: 0, active: true, color: colors[0], position: [xMiddle, yBottom,         0]},
-    {lvl: 1, gear: 0, active: true, color: colors[1], position: [xLeft,   yBottomMiddle,  90]},
-    {lvl: 1, gear: 0, active: true, color: colors[2], position: [xLeft,   yTopMiddle,     90]},
-    {lvl: 1, gear: 0, active: true, color: colors[3], position: [xMiddle, yTop,          180]},
-    {lvl: 1, gear: 0, active: true, color: colors[4], position: [xRight,  yTopMiddle,    270]},
-    {lvl: 1, gear: 0, active: true, color: colors[5], position: [xRight,  yBottomMiddle, 270]}
+    {lvl: 1, gear: 0, active: true , color: $scope.colors[0], position: [xMiddle, yBottom,         0]},
+    {lvl: 1, gear: 0, active: false, color: false, position: [xLeft,   yBottomMiddle,  90]},
+    {lvl: 1, gear: 0, active: false, color: false, position: [xLeft,   yTopMiddle,     90]},
+    {lvl: 1, gear: 0, active: false, color: false, position: [xMiddle, yTop,          180]},
+    {lvl: 1, gear: 0, active: false, color: false, position: [xRight,  yTopMiddle,    270]},
+    {lvl: 1, gear: 0, active: false, color: false, position: [xRight,  yBottomMiddle, 270]}
   ];
 
   $scope.combatants = [];
+
+  // Player seats
+  var player0Pos = [$scope.players[0].position[0], $scope.players[0].position[0] + meepoShift*2, $scope.players[0].position[1], $scope.players[0].position[1] + meepoShift*2];
+  var player1Pos = [$scope.players[1].position[0], $scope.players[1].position[0] + meepoShift*2, $scope.players[1].position[1], $scope.players[1].position[1] + meepoShift*2];
+  var player2Pos = [$scope.players[2].position[0], $scope.players[2].position[0] + meepoShift*2, $scope.players[2].position[1], $scope.players[2].position[1] + meepoShift*2];
+  var player3Pos = [$scope.players[3].position[0], $scope.players[3].position[0] + meepoShift*2, $scope.players[3].position[1], $scope.players[3].position[1] + meepoShift*2];
+  var player4Pos = [$scope.players[4].position[0], $scope.players[4].position[0] + meepoShift*2, $scope.players[4].position[1], $scope.players[4].position[1] + meepoShift*2];
+  var player5Pos = [$scope.players[5].position[0], $scope.players[5].position[0] + meepoShift*2, $scope.players[5].position[1], $scope.players[5].position[1] + meepoShift*2];
 
   // Elements and position functions
   var vsCircle = document.getElementById('vs-icon');
@@ -107,38 +118,78 @@ angular.module('SteroidsApplication', ['supersonic', 'hmTouchEvents'])
   var trashZone = document.getElementById('trash-zone');
   var trashZonePos = [trashZone.offsetLeft, trashZone.offsetLeft + trashZone.offsetWidth, trashZone.offsetTop, trashZone.offsetTop + trashZone.offsetHeight];
 
-  $scope.panOn = false;
+
+  $scope.panMenu = false;
+  $scope.panBench = false;
 
   function areaCalc(positionArr) {
     return positionArr[0] < $scope.mx && $scope.mx < positionArr[1] && positionArr[2] < $scope.my && $scope.my < positionArr[3];
   }
 
-  $scope.positionCheck = function(player) {
-    $scope.panOn = false;
-    if (areaCalc(vsCirclePos)) {
-      combatMove(player);
-    } else if (areaCalc(lvlUpPos)) {
-      player.lvl++;
-    } else if (areaCalc(lvlDownPos)) {
-      player.lvl--;
-    } else if (areaCalc(gearUpPos)) {
-      player.gear++;
-    } else if (areaCalc(gearDownPos)) {
-      player.gear--;
-    } else if (areaCalc(trashZonePos)) {
-      player.active = false;
-      player.lvl = 0;
-      player.gear = 0;
-    };
+  $scope.positionCheck = function(currentObject) {
+    if ($scope.panMenu) {
+      $scope.panMenu = false;
+      if (areaCalc(vsCirclePos)) {
+        combatMove(currentObject);
+      } else if (areaCalc(lvlUpPos)) {
+        currentObject.lvl++;
+      } else if (areaCalc(lvlDownPos)) {
+        currentObject.lvl--;
+      } else if (areaCalc(gearUpPos)) {
+        currentObject.gear++;
+      } else if (areaCalc(gearDownPos)) {
+        currentObject.gear--;
+      } else if (areaCalc(trashZonePos)) {
+        currentObject.active = false;
+        currentObject.lvl = 1;
+        currentObject.gear = 0;
+        currentObject.color[2] = false;
+      };
+    } else if ($scope.panBench) {
+      if (areaCalc(player0Pos)) {
+        $scope.players[0].active = true;
+        $scope.players[0].color[2] = false;
+        $scope.players[0].color = currentObject;
+        currentObject[2] = true;
+      } else if (areaCalc(player1Pos)) {
+        $scope.players[1].active = true;
+        $scope.players[1].color[2] = false;
+        $scope.players[1].color = currentObject;
+        currentObject[2] = true;
+      } else if (areaCalc(player2Pos)) {
+        $scope.players[2].active = true;
+        $scope.players[2].color[2] = false;
+        $scope.players[2].color = currentObject;
+        currentObject[2] = true;
+      } else if (areaCalc(player3Pos)) {
+        $scope.players[3].active = true;
+        $scope.players[3].color[2] = false;
+        $scope.players[3].color = currentObject;
+        currentObject[2] = true;
+      } else if (areaCalc(player4Pos)) {
+        $scope.players[4].active = true;
+        $scope.players[4].color[2] = false;
+        $scope.players[4].color = currentObject;
+        currentObject[2] = true;
+      } else if (areaCalc(player5Pos)) {
+        $scope.players[5].active = true;
+        $scope.players[5].color[2] = false;
+        $scope.players[5].color = currentObject;
+        currentObject[2] = true;
+      };
+      $scope.panBench = false;
+    }
 
     // To avoid cursor jitter
     $scope.mx = -100;
     $scope.my = -100;
   }
 
-  $scope.radialOn = function(player) {
-    $scope.panOn = true;
-    $scope.cursorBg = player.color[0];
+  $scope.radialOn = function(target, panMenu) {
+    $scope.cursorBg = target.color ? target.color[0] : target[0];
+    $scope.panMenu = panMenu;
+    if (!target.color)
+      $scope.panBench = true;
   }
 
   // Cursor
